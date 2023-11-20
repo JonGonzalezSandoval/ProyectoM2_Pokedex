ab8db78a0684f9d85a76342e2e0006bba3ea2bf0
JonGonzalezSandoval <jongonzalezsandoval@gmail.com>
2023-11-20 16:08:46 +0100

    Cosa

--- src/pokemon-types/pokemon-types.controller.ts
+++ src/pokemon-types/pokemon-types.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { PokemonTypesService } from './pokemon-types.service';
 
 @Controller('pokemon-types')
-export class PokemonTypesController {}
+export class PokemonTypesController {
+
+    constructor( private readonly pokemonTypeService: PokemonTypesService){}
+
+    @Get('allTypes')
+    async getTypes(): Promise<any>{
+        try{
+            const allTypes = await this.pokemonTypeService.getAllTypes();
+            return allTypes;
+        }catch(e){
+
+        }
+    }
+
+
+
+}
--- src/pokemon-types/pokemon-types.service.ts
+++ src/pokemon-types/pokemon-types.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { PokemonType, PokemonTypeDocument } from './pokemon-types.schema';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class PokemonTypesService {}
+export class PokemonTypesService {
+
+    constructor(@InjectModel(PokemonType.name) private pokemonTypeModel: Model<PokemonTypeDocument>){}
+
+    async getAllTypes():Promise<any>{
+
+        this.pokemonTypeModel.find()
+    }
+
+
+
+
+}
